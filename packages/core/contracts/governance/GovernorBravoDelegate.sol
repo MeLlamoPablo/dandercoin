9cf7e038ce8b830f8fefbcb4277d12f684c4f00d
Pablo Rodríguez Caballero <pablo@rodriguezcaballero.me>
2021-07-16 19:09:43 +0200

    chore(core): tweak GovernorBravo parameters

--- packages/core/contracts/governance/GovernorBravoDelegate.sol
+++ packages/core/contracts/governance/GovernorBravoDelegate.sol
@@ -8,28 +8,28 @@ import "./GovernorBravoInterfaces.sol";
 contract GovernorBravoDelegate is GovernorBravoDelegateStorageV1, GovernorBravoEvents {
 
   /// @notice The name of this contract
-  string public constant name = "Compound Governor Bravo";
+  string public constant name = "Dandercoin Compound Governor Bravo";
 
   /// @notice The minimum setable proposal threshold
-  uint public constant MIN_PROPOSAL_THRESHOLD = 50000e18; // 50,000 Comp
+  uint public constant MIN_PROPOSAL_THRESHOLD = 1000e18; // 1000 Comp
 
   /// @notice The maximum setable proposal threshold
   uint public constant MAX_PROPOSAL_THRESHOLD = 100000e18; //100,000 Comp
 
   /// @notice The minimum setable voting period
-  uint public constant MIN_VOTING_PERIOD = 5760; // About 24 hours
+  uint public constant MIN_VOTING_PERIOD = 43200; // About 24 hours
 
   /// @notice The max setable voting period
-  uint public constant MAX_VOTING_PERIOD = 80640; // About 2 weeks
+  uint public constant MAX_VOTING_PERIOD = 302400; // About 2 weeks
 
   /// @notice The min setable voting delay
   uint public constant MIN_VOTING_DELAY = 1;
 
   /// @notice The max setable voting delay
-  uint public constant MAX_VOTING_DELAY = 40320; // About 1 week
+  uint public constant MAX_VOTING_DELAY = 151200; // About 1 week
 
   /// @notice The number of votes in support of a proposal required in order for a quorum to be reached and for a vote to succeed
-  uint public constant quorumVotes = 400000e18; // 400,000 = 4% of Comp
+  uint public constant quorumVotes = 30000e18; // 30,000
 
   /// @notice The maximum number of actions that can be included in a proposal
   uint public constant proposalMaxOperations = 10; // 10 actions
