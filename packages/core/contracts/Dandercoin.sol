b6b96da9918900e5998eaa7a32088e9b48b7aa3b
Pablo Rodríguez Caballero <pablo@rodriguezcaballero.me>
2021-07-23 17:19:55 +0200

    feat(core): add burn method to Dandercoin
    This should make the token more flexible as interacting contracts could
    burn the token, accurately reflecting on the total supply.

--- packages/core/contracts/Dandercoin.sol
+++ packages/core/contracts/Dandercoin.sol
@@ -22,6 +22,17 @@ contract Dandercoin is AccessControl, ERC20VotesComp, MintControl {
     _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
   }
 
+  /**
+    * @dev Destroys `amount` tokens from the caller. Allows token holders to
+    * destroy their own tokens, in a way that can be recognized off-chain (via
+    *  event analysis).
+    *
+    * See {ERC20-_burn}.
+    */
+  function burn(uint256 amount) public virtual {
+    _burn(_msgSender(), amount);
+  }
+
   function _canAuthorizeMinter(address account) internal view override returns (bool) {
     return hasRole(DEFAULT_ADMIN_ROLE, account);
   }
--- packages/core/test/Dandercoin.js
+++ packages/core/test/Dandercoin.js
@@ -5,6 +5,8 @@ const MintControl = require('./MintControl');
 const Dandercoin = artifacts.require('Dandercoin');
 
 contract('Dandercoin', (accounts) => {
+  const adminAccount = accounts[0];
+
   it('should transfer coins successfully', async () => {
     const dandercoin = await Dandercoin.deployed();
 
@@ -46,13 +48,30 @@ contract('Dandercoin', (accounts) => {
   it('should have made the deployer admin', async () => {
     const dandercoin = await Dandercoin.deployed();
 
-    const adminAccount = accounts[0];
     const adminRole = await dandercoin.DEFAULT_ADMIN_ROLE.call();
 
     assert.equal(
       await dandercoin.hasRole.call(adminRole, adminAccount),
       true,
       "Admin account wasn't actually an admin",
+    );
+  });
+
+  it('should have made the deployer admin', async () => {
+    const dandercoin = await Dandercoin.deployed();
+
+    const amount = new BN('2000000000000000', 10); // 0.002 DANDER
+
+    const balanceBefore = await dandercoin.balanceOf(adminAccount);
+    await dandercoin.burn(amount, {
+      from: adminAccount,
+    });
+    const balanceAfter = await dandercoin.balanceOf(adminAccount);
+
+    assert.equal(
+      balanceBefore.sub(balanceAfter).eq(amount),
+      true,
+      "Coins weren't burned properly",
     );
   });
 
