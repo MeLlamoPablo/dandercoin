bacfd890bf8e4f66f0f15740b0f947aeb2b7654e
Pablo Rodríguez Caballero <pablo@rodriguezcaballero.me>
2021-07-30 18:22:11 +0200

    feat(core): parametrize quorumVotes in GovernorBravo
    COMP has a fixed supply so for them it makes sense to keep quorumVotes
    as a hardocoded value.
    
    However, because Dandercoin has an inflationary policy, with a hardcoded
    value it'd become increasingly easy to form quorum, so it makes sense
    for us to parametrize it.

--- packages/core/contracts/governance/GovernorBravo.sol
+++ packages/core/contracts/governance/GovernorBravo.sol
@@ -28,9 +28,6 @@ contract GovernorBravo is GovernorBravoStorage, GovernorBravoEvents {
   /// @notice The max setable voting delay
   uint public constant MAX_VOTING_DELAY = 151200; // About 1 week
 
-  /// @notice The number of votes in support of a proposal required in order for a quorum to be reached and for a vote to succeed
-  uint public constant quorumVotes = 30000e18; // 30,000
-
   /// @notice The maximum number of actions that can be included in a proposal
   uint public constant proposalMaxOperations = 10; // 10 actions
 
@@ -174,6 +171,14 @@ contract GovernorBravo is GovernorBravoStorage, GovernorBravoEvents {
   }
 
   /**
+    * @notice Returns the number of votes in support of a proposal required in order for a quorum to be reached and for a vote to succeed
+    */
+  function quorumVotes() public view returns (uint) {
+    // 4% of supply needed to reach quorum
+    return comp.totalSupply() / 25;
+  }
+
+  /**
     * @notice Gets the state of a proposal
     * @param proposalId The id of the proposal
     * @return Proposal state
@@ -187,7 +192,7 @@ contract GovernorBravo is GovernorBravoStorage, GovernorBravoEvents {
       return ProposalState.Pending;
     } else if (block.number <= proposal.endBlock) {
       return ProposalState.Active;
-    } else if (proposal.forVotes <= proposal.againstVotes || proposal.forVotes < quorumVotes) {
+    } else if (proposal.forVotes <= proposal.againstVotes || proposal.forVotes < quorumVotes()) {
       return ProposalState.Defeated;
     } else if (proposal.eta == 0) {
       return ProposalState.Succeeded;
